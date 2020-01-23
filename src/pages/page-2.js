d44405cee9cbf7782480b77b3c00ac0ccff21a06
Sebastian <sebastian.luque.pl@gmail.com>
2020-01-23 01:04:40 -0500

    s

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the ho mepage</Link>
   </DefaultLayout>
 )
 
