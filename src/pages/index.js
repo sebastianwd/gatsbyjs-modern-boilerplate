b626689a45e05d5c8bb5c6e07f16343fa923a90a
Sebastian <sebastian.luque.pl@gmail.com>
2020-01-23 01:06:09 -0500

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => {
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <GatsbyImage />
       </div>
-      <Link to="/page-2/">Go t o pa g e 2</Link>
+      <Link to="/page-2/">G o t o pa g e 2</Link>
     </DefaultLayout>
   )
 }
